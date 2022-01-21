5cc70f7758d04874b19e8de45a2fd3d363cea882
Annie Kuo <ak4f2e@gmail.com>
2022-01-21 14:13:18 +0800

    ipad2

---
+++ ipadAir-2/ipadAir2.js
@@ -0,0 +1,203 @@
+const data = {
+    productName: 'iPad Air',
+    defaultImg: './images/defaultImg.jpeg',
+    spec:[
+        {
+            specName: 'color',
+            specList: ["太空灰色", "銀色", "玫瑰金色", "綠色", "天藍色"]
+        },
+        {
+            specName: 'storage',
+            specList: ["64GB", "256GB"]
+        },
+        {
+            specName: 'network',
+            specList: ["Wi-Fi", "Wi-Fi + 行動網路"]
+        }
+    ],
+    productList: [
+        {
+            "id": "A001",
+            "colorName": "太空灰色",
+            "colorImg": "./images/spacegray.png",
+            "storage": "64GB",
+            "network": "Wi-Fi",
+            "price": 18900,
+            "picture": "./images/select-spacegray.png"
+        },
+        {
+            "id": "A002",
+            "colorName": "銀色",
+            "colorImg": "./images/silver.png",
+            "price": 18900,
+            "storage": "64GB",
+            "network": "Wi-Fi",
+            "picture": "./images/select-silver.png"
+        },
+        {
+            "id": "A003",
+            "colorName": "玫瑰金色",
+            "colorImg": "./images/rose-gold.jpeg",
+            "price": 18900,
+            "storage": "64GB",
+            "network": "Wi-Fi",
+            "picture": "./images/select-gold.png"
+        },
+        {
+            "id": "A004",
+            "colorName": "綠色",
+            "colorImg": "./images/green.jpeg",
+            "price": 18900,
+            "storage": "64GB",
+            "network": "Wi-Fi",
+            "picture": "./images/select-green.png"
+        },
+        {
+            "id": "A005",
+            "colorName": "天藍色",
+            "colorImg": "./images/blue.png",
+            "price": 18900,
+            "storage": "64GB",
+            "network": "Wi-Fi",
+            "picture": "./images/select-blue.png"
+        },
+        {
+            "id": "B001",
+            "colorName": "太空灰色",
+            "colorImg": "./images/spacegray.png",
+            "price": 23900,
+            "storage": "256GB",
+            "network": "Wi-Fi",
+            "picture": "./images/select-spacegray.png"
+        },
+        {
+            "id": "B002",
+            "colorName": "銀色",
+            "colorImg": "./images/silver.png",
+            "price": 23900,
+            "storage": "256GB",
+            "network": "Wi-Fi",
+            "picture": "./images/select-silver.png"
+        },
+        {
+            "id": "B003",
+            "colorName": "玫瑰金色",
+            "colorImg": "./images/rose-gold.jpeg",
+            "price": 23900,
+            "storage": "256GB",
+            "network": "Wi-Fi",
+            "picture": "./images/select-gold.png"
+        },
+        {
+            "id": "B004",
+            "colorName": "綠色",
+            "colorImg": "./images/green.jpeg",
+            "price": 23900,
+            "storage": "256GB",
+            "network": "Wi-Fi",
+            "picture": "./images/select-green.png"
+        },
+        {
+            "id": "B005",
+            "colorName": "天藍色",
+            "colorImg": "./images/blue.png",
+            "price": 23900,
+            "storage": "256GB",
+            "network": "Wi-Fi",
+            "picture": "./images/select-blue.png"
+        },
+        {
+            "id": "C001",
+            "colorName": "太空灰色",
+            "colorImg": "./images/spacegray.png",
+            "price": 23200,
+            "storage": "64GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-spacegray.png"
+        },
+        {
+            "id": "C002",
+            "colorName": "銀色",
+            "colorImg": "./images/silver.png",
+            "price": 23200,
+            "storage": "64GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-silver.png"
+        },
+        {
+            "id": "C003",
+            "colorName": "玫瑰金色",
+            "colorImg": "./images/rose-gold.jpeg",
+            "price": 23200,
+            "storage": "64GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-gold.png"
+        },
+        {
+            "id": "C004",
+            "colorName": "綠色",
+            "colorImg": "./images/green.jpeg",
+            "price": 23200,
+            "storage": "64GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-green.png"
+        },
+        {
+            "id": "C005",
+            "colorName": "天藍色",
+            "colorImg": "./images/blue.png",
+            "price": 23200,
+            "storage": "64GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-blue.png"
+        },
+        {
+            "id": "D001",
+            "colorName": "太空灰色",
+            "colorImg": "./images/spacegray.png",
+            "price": 28200,
+            "storage": "256GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-spacegray.png"
+        },
+        {
+            "id": "D002",
+            "colorName": "銀色",
+            "colorImg": "./images/silver.png",
+            "price": 28200,
+            "storage": "256GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-silver.png"
+        },
+        {
+            "id": "D003",
+            "colorName": "玫瑰金色",
+            "colorImg": "./images/rose-gold.jpeg",
+            "price": 28200,
+            "storage": "256GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-gold.png"
+        },
+        {
+            "id": "D004",
+            "colorName": "綠色",
+            "colorImg": "./images/green.jpeg",
+            "price": 28200,
+            "storage": "256GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-green.png"
+        },
+        {
+            "id": "D005",
+            "colorName": "天藍色",
+            "colorImg": "./images/blue.png",
+            "price": 28200,
+            "storage": "256GB",
+            "network": "Wi-Fi + 行動網路",
+            "picture": "./images/select-blue.png"
+        }
+    ]
+}
+
+
+export {data}
